b56c9028432b1e2b6f5d039ca7cfadf13552e96e
AndreiCherniaev <glscene2010@gmail.com>
2023-02-02 18:03:15 +0900

    remove project build folder

--- test_qt_helloworld/build-test_qt_helloworld/CMakeFiles/test.dir/test_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/test.dir/test_autogen/mocs_compilation.cpp.o: \
- /home/a/Downloads/myGitHub/QT_QEMU_qa_automation/test_qt_helloworld/build-test_qt_helloworld/test_autogen/mocs_compilation.cpp \
- /home/a/Downloads/myGitHub/QT_QEMU_qa_automation/buildroot/output/host/i586-buildroot-linux-gnu/sysroot/usr/include/stdc-predef.h
